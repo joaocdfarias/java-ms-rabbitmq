41604f9217c1ff32cca03d0e3494ddbe5f67f8ec
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2024-09-07 15:20:47 -0300

    feat: adds constructors and repository to proposal service

--- src/main/java/com/joaocdfarias/proposta_app/service/ProposalService.java
+++ src/main/java/com/joaocdfarias/proposta_app/service/ProposalService.java
@@ -1,11 +1,22 @@
 package com.joaocdfarias.proposta_app.service;
 
+import org.springframework.stereotype.Service;
+
 import com.joaocdfarias.proposta_app.dto.ProposalRequestDto;
 import com.joaocdfarias.proposta_app.dto.ProposalResponseDto;
+import com.joaocdfarias.proposta_app.entity.Proposal;
+import com.joaocdfarias.proposta_app.repository.ProposalRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
 public class ProposalService {
 
+  private ProposalRepository proposalRepository;
+
   public ProposalResponseDto create(ProposalRequestDto requestDto) {
+    proposalRepository.save(new Proposal());
     return null;
   }
 }
