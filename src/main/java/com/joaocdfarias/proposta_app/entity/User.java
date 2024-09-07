927ffde157b7d29252ae307a91aee6203c711a39
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2024-09-07 00:40:57 -0300

    feat: creates user entity

---
+++ src/main/java/com/joaocdfarias/proposta_app/entity/User.java
@@ -0,0 +1,30 @@
+package com.joaocdfarias.proposta_app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "\"user\"")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  private String lastName;
+
+  private String cpf;
+
+  private String phone;
+
+  private Double income;
+
+  @OneToOne(mappedBy = "user")
+  private Proposal proposal;
+}
