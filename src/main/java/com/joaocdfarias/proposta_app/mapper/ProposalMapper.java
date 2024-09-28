5dafc908a55742cbdf62d7a9bccf491789ad0004
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2024-09-28 01:34:01 -0300

    feat: adds convertDtoToProposal

--- src/main/java/com/joaocdfarias/proposta_app/mapper/ProposalMapper.java
+++ src/main/java/com/joaocdfarias/proposta_app/mapper/ProposalMapper.java
@@ -2,12 +2,16 @@ package com.joaocdfarias.proposta_app.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 import com.joaocdfarias.proposta_app.dto.ProposalRequestDto;
+import com.joaocdfarias.proposta_app.dto.ProposalResponseDto;
 import com.joaocdfarias.proposta_app.entity.Proposal;
 
 @Mapper
 public interface ProposalMapper {
+
+  ProposalMapper INSTANCE = Mappers.getMapper(ProposalMapper.class);
 
   @Mapping(target = "user.name", source = "name")
   @Mapping(target = "user.lastName", source = "lastName")
@@ -19,5 +23,12 @@ public interface ProposalMapper {
   @Mapping(target = "integrated", ignore = true)
   @Mapping(target = "observation", ignore = true)
   Proposal convertDtoToProposal(ProposalRequestDto proposalRequestDto);
+
+  @Mapping(target = "name", source = "user.name")
+  @Mapping(target = "lastName", source = "user.lastName")
+  @Mapping(target = "cpf", source = "user.cpf")
+  @Mapping(target = "phone", source = "user.phone")
+  @Mapping(target = "income", source = "user.income")
+  ProposalResponseDto convertEntityToDto(Proposal proposal);
 
 }
--- src/main/java/com/joaocdfarias/proposta_app/service/ProposalService.java
+++ src/main/java/com/joaocdfarias/proposta_app/service/ProposalService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import com.joaocdfarias.proposta_app.dto.ProposalRequestDto;
 import com.joaocdfarias.proposta_app.dto.ProposalResponseDto;
 import com.joaocdfarias.proposta_app.entity.Proposal;
+import com.joaocdfarias.proposta_app.mapper.ProposalMapper;
 import com.joaocdfarias.proposta_app.repository.ProposalRepository;
 
 import lombok.AllArgsConstructor;
@@ -16,7 +17,9 @@ public class ProposalService {
   private ProposalRepository proposalRepository;
 
   public ProposalResponseDto create(ProposalRequestDto requestDto) {
-    proposalRepository.save(new Proposal());
-    return null;
+    Proposal proposal = ProposalMapper.INSTANCE.convertDtoToProposal(requestDto);
+    proposalRepository.save(proposal);
+
+    return ProposalMapper.INSTANCE.convertEntityToDto(proposal);
   }
 }
